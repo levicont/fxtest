eada8a802b65befe3636c9858849f5df6cd826aa
levicont <victor84lg@mail.ru>
2017-01-03 16:06:10 +0200

    Added content bias sample

--- src/main/java/com/lvg/fxtest/bounds/LayoutBoundsSample.java
+++ src/main/java/com/lvg/fxtest/bounds/LayoutBoundsSample.java
@@ -6,17 +6,29 @@ import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Shadow;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Shear;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Victor Levchenko LVG Corp. on 30.12.16.
  */
 public class LayoutBoundsSample extends Application {
+    private static final Logger LOGGER = Logger.getLogger(LayoutBoundsSample.class);
+
+    private Button btMove;
+    private Button btBias;
+    private Button btEnableBias;
+    private Rectangle rect;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -27,11 +39,11 @@ public class LayoutBoundsSample extends Application {
     public void start(Stage primaryStage) throws Exception {
         VBox root = new VBox();
 
-        Rectangle rect = new Rectangle(0, 0, 70, 50);
+        rect = new Rectangle(0, 0, 70, 50);
 
         initRectangle(rect);
 
-        root.getChildren().addAll(rect);
+        root.getChildren().addAll(rect, btMove, btBias, btEnableBias);
 
         Scene scene = new Scene(root, 600, 400);
 
@@ -46,5 +58,33 @@ public class LayoutBoundsSample extends Application {
         rectangle.setEffect(new DropShadow());
     }
 
+    @Override
+    public void init() throws Exception {
+        rect = new Rectangle(0, 0, 70, 50);
 
+        initRectangle(rect);
+
+        btMove = new Button("Move");
+        btMove.setOnAction(e -> rect.getTransforms().addAll(new Translate(100, 80), new Rotate(30), new Scale(1.2,1.2),
+                new Shear(1.2,1.2)));
+
+        btBias = new Button("JavaFX New Application");
+        btEnableBias = new Button("Enable");
+        btEnableBias.setOnAction(e -> {
+            if (btBias.getContentBias()!= null){
+                btBias.setWrapText(false);
+                btBias.setPrefWidth(-1);
+                LOGGER.info("Content bias of BiasButton: "+btBias.getContentBias());
+
+            }
+            else {
+                btBias.setWrapText(true);
+                btBias.setPrefWidth(40);
+                LOGGER.info("Content bias of BiasButton: "+btBias.getContentBias());
+            }
+                }
+        );
+
+
+    }
 }
